3b40e7dc50c12932e6a9404d0fbaf555fff42fce
Jonathan <deroo.jon@gmail.com>
2024-06-17 14:51:00 +0200

    First version

---
+++ src/main/java/be/intec/employeemanager/Service/EmployeeService.java
@@ -0,0 +1,44 @@
+package be.intec.employeemanager.Service;
+
+import be.intec.employeemanager.exception.UserNotFoundException;
+import be.intec.employeemanager.model.Employee;
+import be.intec.employeemanager.repository.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+
+    private final EmployeeRepo employeeRepo;
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    public Employee addEmployee(Employee employee) {
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+
+    public List<Employee> findAllEmployees() {
+        return employeeRepo.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        return employeeRepo.save(employee);
+    }
+
+    public Employee findEmployeeById(Long id) {
+        return employeeRepo.findEmployeeById(id).orElseThrow(() -> new UserNotFoundException("User by id " + id + " was not found"));
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepo.deleteEmployeeById(id);
+    }
+}
+
+
